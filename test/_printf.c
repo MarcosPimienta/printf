6671515e87909a7158d58e699eb9a336c8a24e78
MarcosPimienta <1676@holbertonschool.com>
2020-03-18 04:01:46 +0000

    trying

--- test/_printf.c
+++ test/_printf.c
@@ -1,49 +1,47 @@
-#include <limits.h>
 #include <stdio.h>
 #include "holberton.h"
 #include <unistd.h>
 #include <stdarg.h>
-
 /**
- * main - Entry point
- *
+ * _printf - prints almost ewverything that it gets sent to
+ * @format: pointer that gets data to this script
+ * @functions: list functions to the script in a variadic function
  * Return: Always 0
  */
 int _printf(const char *format, ...)
 {
-        va_list functions;
-        int iptf = 0, len = 0;
-        if (format == NULL)
-                return (-1);
-        va_start(functions, format);
-        while (format && format[iptf] != '\0')
-        {
-                if(format[iptf] == '%')
-                {
- 
-                    if (format[++iptf]  == '%')
-                    {
-                      _putchar(format[iptf]);
-                      len++;
-                    }
-         	   len = len + (*get_fun(&format[iptf]))(functions);
-   
-         }
-                else if (format[iptf] == '%')
-                {
-           /** len = len + (*get_fun(format[iptf + 1])) (functions);*/
-                    _putchar('%');
+	va_list functions;
+	int iptf = 0, len = 0;
+
+	if (format == NULL)
+	return (-1);
+	va_start(functions, format);
+	while (format && format[iptf] != '\0')
+	{
+		if (format[iptf] == '%')
+		{
+			if (format[++iptf]  == '%')
+			{
 			_putchar(format[iptf]);
-                    len = len + 2;
-                }
-                else
-                {
-                  _putchar(format[iptf]);
-                  len++;
-                  }
-            iptf++;
-        }
-        va_end(functions);
-	printf("----%d----\n", iptf);
-        return (iptf);
+			len++;
+			}
+				if (format[iptf] == 'c' || format[iptf] == 's'
+					|| format[iptf] == 'd' || format[iptf] == 'i')
+				len = len + (*get_fun(&format[iptf]))(functions);
+		}
+					else if (format[iptf] == '%')
+					{
+						_putchar('%');
+						_putchar(format[iptf]);
+						len = len + 2;
+					}
+					else
+					{
+						_putchar(format[iptf]);
+						len++;
+					}
+				iptf++;
+	}
+	va_end(functions);
+	return (iptf);
 }
